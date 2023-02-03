51238b520e78e486e015d28159a20b0926bb8a10
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-03 06:11:19 +0300

    modify files. Send and receive data from API. Render input data on page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-import './style.css';
+import { addGame, getGame } from "./modules/api.js";
+
+const formSubmit = document.querySelector(".input-form");
+const refresh = document.querySelector(".refresh-btn");
+let nameInput = document.getElementById("name-input");
+let scoreInput = document.getElementById("score-input");
+
+const renderGame = async () => {
+  const outputContainer = document.querySelector(".output-list");
+  outputContainer.innerHTML = "";
+  const getResult = await getGame();
+  getResult.result.forEach((res) => {
+    const html = `
+    <li class="output-li">${res.user}:${res.score}</li>
+    `;
+    outputContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+formSubmit.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const user = nameInput.value;
+  const score = scoreInput.value;
+  if (!user || !score) return;
+  const newscore = {
+    user,
+    score,
+  };
+  addGame(newscore);
+  formSubmit.reset();
+});
+refresh.addEventListener("click", (e) => {
+  e.preventDefault();
+  renderGame();
+});
+document.addEventListener("DOMContentLoaded", async () => {
+  renderGame();
+});
