eb33bec61d4f3c01de598201d29ceebcf61da702
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-03 06:23:41 +0300

    load css from javascript file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import { addGame, getGame } from './modules/api.js';
+import "./style.css";
+import { addGame, getGame } from "./modules/api.js";
 
-const formSubmit = document.querySelector('.input-form');
-const refresh = document.querySelector('.refresh-btn');
-const nameInput = document.getElementById('name-input');
-const scoreInput = document.getElementById('score-input');
+const formSubmit = document.querySelector(".input-form");
+const refresh = document.querySelector(".refresh-btn");
+const nameInput = document.getElementById("name-input");
+const scoreInput = document.getElementById("score-input");
 
 const renderGame = async () => {
-  const outputContainer = document.querySelector('.output-list');
-  outputContainer.innerHTML = '';
+  const outputContainer = document.querySelector(".output-list");
+  outputContainer.innerHTML = "";
   const getResult = await getGame();
   getResult.result.forEach((res) => {
     const html = `
     <li class="output-li">${res.user}:${res.score}</li>
     `;
-    outputContainer.insertAdjacentHTML('beforeend', html);
+    outputContainer.insertAdjacentHTML("beforeend", html);
   });
 };
 
-formSubmit.addEventListener('submit', async (e) => {
+formSubmit.addEventListener("submit", async (e) => {
   e.preventDefault();
   const user = nameInput.value;
   const score = scoreInput.value;
@@ -29,10 +30,10 @@ formSubmit.addEventListener('submit', async (e) => {
   addGame(newscore);
   formSubmit.reset();
 });
-refresh.addEventListener('click', (e) => {
+refresh.addEventListener("click", (e) => {
   e.preventDefault();
   renderGame();
 });
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   renderGame();
 });
