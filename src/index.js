45da5788748f8887f8e81a15a611f68bb6268c0b
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-03 06:18:28 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import { addGame, getGame } from "./modules/api.js";
+import { addGame, getGame } from './modules/api.js';
 
-const formSubmit = document.querySelector(".input-form");
-const refresh = document.querySelector(".refresh-btn");
-let nameInput = document.getElementById("name-input");
-let scoreInput = document.getElementById("score-input");
+const formSubmit = document.querySelector('.input-form');
+const refresh = document.querySelector('.refresh-btn');
+const nameInput = document.getElementById('name-input');
+const scoreInput = document.getElementById('score-input');
 
 const renderGame = async () => {
-  const outputContainer = document.querySelector(".output-list");
-  outputContainer.innerHTML = "";
+  const outputContainer = document.querySelector('.output-list');
+  outputContainer.innerHTML = '';
   const getResult = await getGame();
   getResult.result.forEach((res) => {
     const html = `
     <li class="output-li">${res.user}:${res.score}</li>
     `;
-    outputContainer.insertAdjacentHTML("beforeend", html);
+    outputContainer.insertAdjacentHTML('beforeend', html);
   });
 };
 
-formSubmit.addEventListener("submit", async (e) => {
+formSubmit.addEventListener('submit', async (e) => {
   e.preventDefault();
   const user = nameInput.value;
   const score = scoreInput.value;
@@ -29,10 +29,10 @@ formSubmit.addEventListener("submit", async (e) => {
   addGame(newscore);
   formSubmit.reset();
 });
-refresh.addEventListener("click", (e) => {
+refresh.addEventListener('click', (e) => {
   e.preventDefault();
   renderGame();
 });
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener('DOMContentLoaded', async () => {
   renderGame();
 });
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,20 +1,20 @@
 const addGame = async (newScore) => {
   const result = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/  5tZeFvL8uot4ml3hfz7h/scores",
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/  5tZeFvL8uot4ml3hfz7h/scores',
     {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify(newScore),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
-    }
+    },
   );
   const finalResult = result.json();
   return finalResult;
 };
 const getGame = async () => {
   const data = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/  5tZeFvL8uot4ml3hfz7h/scores"
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/  5tZeFvL8uot4ml3hfz7h/scores',
   );
   const finalData = await data.json();
   return finalData;
